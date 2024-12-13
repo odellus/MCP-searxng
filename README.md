740c5a7fe4b24b324c9641f3140223c2ea2ace4f
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-12-13 17:39:57 +0000

    add initial server implementation

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # MCP-searxng
 MCP server for connecting agentic systems to search systems via searXNG
+
+## Usage
+
+1) Add the server to claude desktop (the entrypoint is main.py)
+
+2) set the environment variable `SEARXNG_URL` to the url of the searxng server (default is `http://localhost:8080`)
+
+3) run claude desktop
---
+++ mcp-searxng/main.py
@@ -0,0 +1,27 @@
+from mcp.server import NotificationOptions
+from mcp.server.models import InitializationOptions
+import mcp.server.stdio
+
+from server import server
+import prompts
+import tools
+
+async def run():
+    # Run the server as STDIO
+    async with mcp.server.stdio.stdio_server() as (read_stream, write_stream):
+        await server.run(
+            read_stream,
+            write_stream,
+            InitializationOptions(
+                server_name="example",
+                server_version="0.1.0",
+                capabilities=server.get_capabilities(
+                    notification_options=NotificationOptions(),
+                    experimental_capabilities={},
+                )
+            )
+        )
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(run())
